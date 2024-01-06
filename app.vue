2ba3d05008fa33aaa2de25c3f13267a01def9217
userquin <userquin@gmail.com>
2024-01-06 16:17:58 +0100

    chore: update icon set and include some vuetify components

--- app.vue
+++ app.vue
@@ -3,7 +3,7 @@ import { PhHorse } from '@phosphor-icons/vue'
 </script>
 
 <template>
-  <div>
+  <VContainer>
     <PhHorse />
     <VBtn color="success" :prepend-icon="PhHorse">
       Custom PhHorse Icon
@@ -11,5 +11,25 @@ import { PhHorse } from '@phosphor-icons/vue'
     <VBtn color="success" prepend-icon="heart">
       PhHeart via Vuetify Alias
     </VBtn>
-  </div>
+
+    <v-select
+      label="Select"
+      :items="[
+        'California',
+        'Colorado',
+        'Florida',
+        'Georgia',
+        'Texas',
+        'Wyoming',
+      ]"
+    />
+
+    <VCheckbox label="Checkbox" />
+
+    <VRadioGroup>
+      <VRadio label="Radio One" value="one" />
+      <VRadio label="Radio Two" value="two" />
+      <VRadio label="Radio Three" value="three" />
+    </VRadioGroup>
+  </VContainer>
 </template>
--- phosphor/index.ts
+++ phosphor/index.ts
@@ -1,5 +1,16 @@
 import type { IconAliases, IconSet } from 'vuetify'
 import { h, resolveComponent } from 'vue'
+import {
+  PhArrowDown,
+  PhCalendar,
+  PhCheckSquare,
+  PhCheckSquareOffset,
+  PhCircle,
+  PhHeart,
+  PhHorse,
+  PhRadioButton,
+  PhSquare,
+} from '@phosphor-icons/vue'
 
 function toKebabCase(str = '') {
   if (toKebabCase.cache.has(str))
@@ -19,45 +30,45 @@ toKebabCase.cache = new Map<string, string>()
 
 const aliases = <IconAliases>{
   /* custom ones */
-  heart: 'PhHeart',
-  horse: 'PhHorse',
+  heart: PhHeart,
+  horse: PhHorse,
   /* vuetify aliases */
-  calendar: 'PhCalendar',
-  cancel: 'PhCircle',
-  checkboxIndeterminate: 'PhCheckSquareOffset',
-  checkboxOn: 'PhCheckSquare',
-  checkboxOff: 'PhSquare',
-  clear: 'PhCircle',
+  calendar: PhCalendar,
+  cancel: PhCircle,
+  checkboxIndeterminate: PhCheckSquareOffset,
+  checkboxOn: PhCheckSquare,
+  checkboxOff: PhSquare,
+  clear: PhCircle,
   /* more vuetify aliases here: SHOULD BE CHANGED */
-  close: 'PhCircle',
-  complete: 'PhCircle',
-  delete: 'PhCircle',
-  delimiter: 'PhCircle',
-  dropdown: 'PhCircle',
-  edit: 'PhCircle',
-  error: 'PhCircle',
-  expand: 'PhCircle',
-  file: 'PhCircle',
-  first: 'PhCircle',
-  info: 'PhCircle',
-  last: 'PhCircle',
-  loading: 'PhCircle',
-  menu: 'PhCircle',
-  minus: 'PhCircle',
-  next: 'PhCircle',
-  plus: 'PhCircle',
-  prev: 'PhCircle',
-  radioOff: 'PhCircle',
-  radioOn: 'PhCircle',
-  ratingEmpty: 'PhCircle',
-  ratingFull: 'PhCircle',
-  ratingHalf: 'PhCircle',
-  sortAsc: 'PhCircle',
-  sortDesc: 'PhCircle',
-  subgroup: 'PhCircle',
-  success: 'PhCircle',
-  unfold: 'PhCircle',
-  warning: 'PhCircle',
+  close: PhCircle,
+  complete: PhCircle,
+  delete: PhCircle,
+  delimiter: PhCircle,
+  subgroup: PhArrowDown,
+  dropdown: PhArrowDown,
+  edit: PhCircle,
+  error: PhCircle,
+  expand: PhCircle,
+  file: PhCircle,
+  first: PhCircle,
+  info: PhCircle,
+  last: PhCircle,
+  loading: PhCircle,
+  menu: PhCircle,
+  minus: PhCircle,
+  next: PhCircle,
+  plus: PhCircle,
+  prev: PhCircle,
+  radioOff: PhCircle,
+  radioOn: PhRadioButton,
+  ratingEmpty: PhCircle,
+  ratingFull: PhCircle,
+  ratingHalf: PhCircle,
+  sortAsc: PhCircle,
+  sortDesc: PhCircle,
+  success: PhCircle,
+  unfold: PhCircle,
+  warning: PhCircle,
 }
 
 const phosphor = <IconSet>{
@@ -68,8 +79,9 @@ const phosphor = <IconSet>{
     const { icon, ...rest } = props
     const stringIcon = icon as string
     return h(props.tag, rest, [
-      h(resolveComponent(toKebabCase(aliases[icon as string])), {
+      h(aliases[stringIcon] ?? resolveComponent(toKebabCase(stringIcon)), {
         key: stringIcon,
+        ...rest,
       }),
     ])
   },
