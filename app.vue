457cf0a191429715e8d0d9ea2fc4670aa579dcc6
userquin <userquin@gmail.com>
2024-01-06 15:33:57 +0100

    chore: fix lint

--- app.vue
+++ app.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
-import { PhHorse } from '@phosphor-icons/vue';
+import { PhHorse } from '@phosphor-icons/vue'
 </script>
+
 <template>
   <div>
     <PhHorse />
-    <VBtn color="success" :prepend-icon="PhHorse">Custom PhHorse Icon</VBtn>
-    <VBtn color="success" prepend-icon="heart"
-      >PhHeart via Vuetify Alias
+    <VBtn color="success" :prepend-icon="PhHorse">
+      Custom PhHorse Icon
+    </VBtn>
+    <VBtn color="success" prepend-icon="heart">
+      PhHeart via Vuetify Alias
     </VBtn>
   </div>
 </template>
